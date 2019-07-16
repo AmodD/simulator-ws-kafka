1d130be0e6cc61a1197ec615e0bcffb850692ffd
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2019-07-16 07:48:20 +0530

    removed warnings

--- src/main/java/com/fortiate/simulatorwskafka/TickService.java
+++ src/main/java/com/fortiate/simulatorwskafka/TickService.java
@@ -3,9 +3,7 @@ package com.fortiate.simulatorwskafka;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
